5695152677f5dbecaa0ffe4ba3614b55e6ccaf66
Artem Murzo <murzotom@rocketmail.com>
2024-09-19 18:29:28 -0400

    imagepicker types all

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -22,7 +22,7 @@ export default function Create() {
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       // aspect: [4, 3],
       // quality: 0,
