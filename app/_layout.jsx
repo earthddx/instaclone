1317331dc9b56b01cedcf5fecab3f0f145ae0267
Artem Murzo <murzotom@rocketmail.com>
2026-02-22 11:24:43 -0500

    app layout wrapped by error boundary

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,20 +4,23 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import UserProvider from "../context/UserProvider";
 import { ToastProvider } from "../context/ToastContext";
 import Toast from "../components/Toast";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <UserProvider>
         <ToastProvider>
-          <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="user" options={{ headerShown: false }} />
-            <Stack.Screen name="post" options={{ headerShown: false }} />
-          </Stack>
-          <Toast />
+          <ErrorBoundary>
+            <Stack>
+              <Stack.Screen name="index" options={{ headerShown: false }} />
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="user" options={{ headerShown: false }} />
+              <Stack.Screen name="post" options={{ headerShown: false }} />
+            </Stack>
+            <Toast />
+          </ErrorBoundary>
         </ToastProvider>
       </UserProvider>
     </GestureHandlerRootView>
